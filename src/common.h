d9481f4532ecb33c4f054c8cfb7530a742f73891
paweu9000 <wolfenorrin5@gmail.com>
2024-09-16 18:02:40 +0200

    initial commit

---
+++ src/common.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <glad.h>
+#include <glfw3.h>
+#include <iostream>
---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include "common.h"
+
+int main() {
+    if (!glfwInit()) std::cout << "ERROOOOR";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Game", nullptr, nullptr);
+    if (window == nullptr) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize glad\n";
+    }
+
+    glViewport(0, 0, 800, 600);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
