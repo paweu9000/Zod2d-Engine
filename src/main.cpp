0ae9621abc3724b926cdff9b2adb2d2f01cb63c9
paweu9000 <wolfenorrin5@gmail.com>
2024-09-27 17:17:03 +0200

    basic logger implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
 #include "common.h"
 
 int main() {
-    if (!glfwInit()) std::cout << "ERROOOOR";
+    if (!glfwInit()) G_ERROR("GLFW failed to load");
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     GLFWwindow* window = glfwCreateWindow(800, 600, "Game", nullptr, nullptr);
     if (window == nullptr) {
-        std::cout << "Failed to create GLFW window" << std::endl;
+        G_ERROR("Failed to create window");
         glfwTerminate();
         return -1;
     }
@@ -16,7 +16,7 @@ int main() {
     glfwMakeContextCurrent(window);
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        std::cout << "Failed to initialize glad\n";
+        G_ERROR("Failed to load GLAD");
     }
 
     glViewport(0, 0, 800, 600);
