167137dc80d48f8234c4a90a89d8914add6e757d
paweu9000 <wolfenorrin5@gmail.com>
2024-10-10 19:33:10 +0200

    done

--- src/engine.cpp
+++ src/engine.cpp
@@ -3,17 +3,13 @@
 #include "api/gl_backend.h"
 #include "api/gl_renderer.h"
 #include <vector>
+#include "utils.hpp"
 
-std::vector<Vertex> vertices = {
-	Vertex(glm::vec3(0.5f, 0.5f, 0.0f), glm::vec2(1.0f, 1.0f)),
-	Vertex(glm::vec3(0.5f, -0.5f, 0.0f), glm::vec2(1.0f, 0.0f)),
-	Vertex(glm::vec3(-0.5f, -0.5f, 0.0f), glm::vec2(0.0f, 0.0f)),
-	Vertex(glm::vec3(-0.5f, 0.5f, 0.0f), glm::vec2(0.0f, 1.0f))
-};
+glm::vec2 pos{900.f, 500.f};
 
 std::vector<uint32_t> indices = {
 	0, 1, 3,
-	1, 2, 3
+	0, 3, 2
 };
 
 void Engine::run() 
@@ -25,7 +21,7 @@ void Engine::run()
 	while (Backend::is_running()) {
 		Backend::begin_frame();
 		GLBackend::rebind_buffers();
-		GLRenderer::render_frame(vertices, indices);
+		GLRenderer::render_frame(Utils::get_pos(pos, 64), indices);
 		Backend::end_frame();
 	}
 
